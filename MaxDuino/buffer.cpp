37ec0f0c5372273d013f6414acff55638e9c64d4
Dave Hooper <dave@beermex.com>
2026-04-26 15:09:47 +0100

    change all the buffer and isr to use word (uint16_t) instead of byte manipulation. since everything is unified and EVERYTHING uses 2x bytes , might as well do it properly now and make it a word array instead of a byte array.  OTLA performance seems unchanged and firmware is smaller.  Add support for buffsize==256 (and enable it for some devices with lots of RAM). Note that if buffsize==256 then a lot of readpos/writepos comparisons are simpler which makes firmware slightly smaller than (say) buffsize=254.  typedef the necessary types for buffersizes > 256 bytes i.e. uint16_t .

--- MaxDuino/buffer.cpp
+++ MaxDuino/buffer.cpp
@@ -1,19 +1,28 @@
 #include "buffer.h"
 
 volatile bool morebuff = false;
-byte readpos = 0; // only used within the ISR, never accessed outside, so doesn't need to be volatile
-byte writepos = 0; // only used within the main loop, never accessed by ISR, so doesn't need to be volatile
-volatile byte wbuffer[2][buffsize];
-volatile byte * volatile writeBuffer=wbuffer[0]; // the pointer itself is volatile (since the ISR can swap readBuffer/writeBuffer)
-volatile byte * readBuffer=wbuffer[1]; // this pointer is not volatile since this pointer is only manipulated by the ISR, not code outside the ISR
+buffsize_t readpos = 0; // only used within the ISR, never accessed outside, so doesn't need to be volatile
+buffsize_t writepos = 0; // only used within the main loop, never accessed by ISR, so doesn't need to be volatile
+bool write_buffer_full = false;
+volatile uint16_t wbuffer[2][buffsize];
+volatile uint16_t * volatile writeBuffer=wbuffer[0]; // the pointer itself is volatile (since the ISR can swap readBuffer/writeBuffer)
+volatile uint16_t * readBuffer=wbuffer[1]; // this pointer is not volatile since this pointer is only manipulated by the ISR, not code outside the ISR
 
 void clearBuffer(void)
 {
   noInterrupts();
-  for(byte i=0;i<buffsize;i++)
+  buffsize_t i=buffsize-1;
+  do
   {
     wbuffer[0][i]=0;
     wbuffer[1][i]=0;
-  }
+  } while (i--);
   interrupts();
+}
+
+void advance_write_word()
+{
+  writepos++;
+  if (writepos==(buffsize_t)(buffsize))  // this handles overflow correctly i.e. if buffsize==256 and writepos has wrapped from 255 to 0, (buffsize_t)(buffsize) == 0 too.
+    write_buffer_full=true;
 }
--- MaxDuino/buffer.h
+++ MaxDuino/buffer.h
@@ -4,20 +4,45 @@
 #include "Arduino.h" // for types
 #include "configs.h"
 
-/* With latest casprocessing logic, buffsize can be any multiple of 2.
-*/
-#ifdef LARGEBUFFER
-  #define buffsize 254
+/* buffsize can be any number now, the larger the better .
+It is defined in terms of number of words i.e. uint16_t
+(so, the number of bytes needed is twice that, per buffer.  And there are two
+buffers, so in total you need this number x 4 in free ram)
+However, some loops and optimizations make assumptions that buffsize >= 8
+so 8 is the absolute minimum so let's say we only support >= 32 words buffer.
+I can't imagine anyone wanting (or needing) the buffer tot be that small though.  */
+
+#if defined(BUFFER_SIZE)
+  #define buffsize BUFFER_SIZE  // custom number of words
 #else
-  #define buffsize 176
+  #if defined(VERY_LARGE_BUFFER)
+    #define buffsize 512  // 1024 bytes
+  #elif defined(LARGEBUFFER)
+    #define buffsize 128  // 256 bytes
+  #else
+    #define buffsize 88  // 176 bytes
+  #endif
+#endif
+
+#if buffsize < 32
+#error buffsize needs be at least 32 words (64 bytes)! And you probably want it to be as large as possible and greater than 64 words (128 bytes)!
+#endif
+
+// we need to use an appropriate type to index this array.  uint8_t can index into an up to and including 256 entries (0-255).  if buffsize>256 then you need a larger type.
+#if buffsize <= 256  
+  typedef uint8_t buffsize_t;
+#else
+  typedef uint16_t buffsize_t;
 #endif
 
 extern volatile bool morebuff;
-extern byte readpos;
-extern byte writepos;
-extern volatile byte wbuffer[2][buffsize];
-extern volatile byte * volatile writeBuffer;
-extern volatile byte * readBuffer;
+extern buffsize_t readpos;
+extern buffsize_t writepos;
+extern bool write_buffer_full;
+extern volatile uint16_t wbuffer[2][buffsize];
+extern volatile uint16_t * volatile writeBuffer;
+extern volatile uint16_t * readBuffer;
 void clearBuffer(void);
+void advance_write_word(void);
 
 #endif // BUFFER_H_INCLUDED
