3ebf37329da71d7ee4e43c0da88251f1b805a13c
Dave Hooper <dave@beermex.com>
2026-07-18 00:35:12 +0100

    Simplify i2c configuration and add another library, Acewire, which is smaller than SoftI2CMaster and just as quick (when using avr optimisations).  Made Acewire the default on nano/32u4/mega.

--- MaxDuino/i2c.h
+++ MaxDuino/i2c.h
@@ -19,51 +19,55 @@
   #endif
 #endif
 
-#if defined(__AVR_ATmega2560__) || defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega328P__)
-  #if I2C_Library_Preference == _I2C_Impl_SoftI2CMaster
-    #define I2C_Library_Used (_I2C_Impl_SoftI2CMaster)
-  #elif I2C_Library_Preference == _I2C_Impl_SoftWire
-    #define I2C_Library_Used (_I2C_Impl_SoftWire)
+
+#if (I2C_Library_Preference == _I2C_Impl_SoftI2CMaster)
+  // these #defines in the section are ONLY used by the SoftI2CMaster library
+  #if (defined(__AVR_ATmega2560__) || defined(__AVR_ATmega32U4__))
+    #define SDA_PORT PORTD
+    #define SDA_PIN 1 
+    #define SCL_PORT PORTD
+    #define SCL_PIN 0 
   #else
-    #warning Wire library uses a lot of firmware space, you would be better off using SoftI2CMaster
-    #define I2C_Library_Used (_I2C_Impl_Wire)
+    #define SDA_PORT PORTC
+    #define SDA_PIN 4 
+    #define SCL_PORT PORTC
+    #define SCL_PIN 5
   #endif
-#else
-  #if I2C_Library_Preference == _I2C_Impl_SoftI2CMaster
-    #warning This chip does not support SoftI2CMaster. Using default.
-  #endif
-  #if I2C_Library_Preference == _I2C_Impl_SoftWire
-    #warning This chip does not support SoftWire. Using default.
-  #endif
-  #define I2C_Library_Used (_I2C_Impl_Wire)
 #endif
 
-#if (defined(__AVR_ATmega2560__) || defined(__AVR_ATmega32U4__))
-  #define SDA_PORT PORTD
-  #define SDA_PIN 1 
-  #define SCL_PORT PORTD
-  #define SCL_PIN 0 
-#else
-  #define SDA_PORT PORTC
-  #define SDA_PIN 4 
-  #define SCL_PORT PORTC
-  #define SCL_PIN 5
-//  #define I2C_HARDWARE 1   // requires more thorough testing. Needs additional delay in init_OLED e.g. 100ms after sendcommand(0xAF)
+
+#if (I2C_Library_Preference == _I2C_Impl_AceWire)
+  #ifndef I2C_DELAY_MICROS
+    #define I2C_DELAY_MICROS ((1000000UL / (2UL * I2CCLOCK)) > 0 ? (1000000UL / (2UL * I2CCLOCK)) : 1)
+  #endif
 #endif
 
-#if (I2C_Library_Used == _I2C_Impl_SoftI2CMaster)
-  // ideally I'd do this:
+
+#if (I2C_Library_Preference == _I2C_Impl_AceWire) && defined(ARDUINO_ARCH_AVR)
+  #include <digitalWriteFast.h>
+  #include <ace_wire/SimpleWireFastInterface.h>
+  using ace_wire::SimpleWireFastInterface;
+  extern SimpleWireFastInterface<SDA, SCL, I2C_DELAY_MICROS> i2cAceWire;
+  #define mx_i2c_init() i2cAceWire.begin()
+  #define mx_i2c_start(address) i2cAceWire.beginTransmission(address)
+  #define mx_i2c_write(byte) i2cAceWire.write(byte)
+  #define mx_i2c_end() i2cAceWire.endTransmission()
+
+#elif (I2C_Library_Preference == _I2C_Impl_SoftI2CMaster)
+
+// ideally I'd do this:
   //     #define USE_SOFT_I2C_MASTER_H_AS_PLAIN_INCLUDE
   //     #include <SoftI2CMaster.h>
   // but as of 2.1.9 it appears it still doesn't work properly (at least not with platform io)
   // so I'll forward-declare everything myself:
-  #ifndef I2C_WRITE
-  #define I2C_WRITE 0
-  #endif
   bool __attribute__ ((noinline)) i2c_init(void) __attribute__ ((used));
   bool __attribute__ ((noinline)) i2c_start(uint8_t addr) __attribute__ ((used));
   void __attribute__ ((noinline)) i2c_stop(void) asm("ass_i2c_stop") __attribute__ ((used));
   bool __attribute__ ((noinline)) i2c_write(uint8_t value) asm("ass_i2c_write") __attribute__ ((used));
+
+  #ifndef I2C_WRITE
+  #define I2C_WRITE 0
+  #endif
 
   #define mx_i2c_init() i2c_init()
   #define mx_i2c_start(address) i2c_start((address<<1)|I2C_WRITE)
@@ -71,11 +75,15 @@
   #define mx_i2c_end() i2c_stop()
 
 #else
-  // Wire or SoftWire
-  #if (I2C_Library_Used == _I2C_Impl_SoftWire)
+  // Wire, SoftWire, or AceWire (non-AVR)
+  #if (I2C_Library_Preference == _I2C_Impl_SoftWire)
     extern SoftWire Wire;
-  #elif (I2C_Library_Used == _I2C_Impl_Wire)
+  #elif (I2C_Library_Preference == _I2C_Impl_Wire)
     #include <Wire.h>
+  #elif (I2C_Library_Preference == _I2C_Impl_AceWire)
+    #include <AceWire.h>
+    using ace_wire::SimpleWireInterface;
+    extern SimpleWireInterface i2cAceWire;
   #else
     #error Unknown I2C library configuration
   #endif
@@ -93,15 +101,23 @@
     #define I2C_WIRE_CLASS Wire
   #endif
 
-  #define mx_i2c_init() I2C_WIRE_CLASS.begin();\
-                        I2C_WIRE_CLASS.setClock(I2CCLOCK)
-  #define mx_i2c_start(address) I2C_WIRE_CLASS.beginTransmission(address)
-  #define mx_i2c_write(byte) I2C_WIRE_CLASS.write(byte)
-  #define mx_i2c_end() I2C_WIRE_CLASS.endTransmission()
+  #if (I2C_Library_Preference == _I2C_Impl_Wire) || (I2C_Library_Preference == _I2C_Impl_SoftWire)
+    #define mx_i2c_init() I2C_WIRE_CLASS.begin();\
+                          I2C_WIRE_CLASS.setClock(I2CCLOCK)
+    #define mx_i2c_start(address) I2C_WIRE_CLASS.beginTransmission(address)
+    #define mx_i2c_write(byte) I2C_WIRE_CLASS.write(byte)
+    #define mx_i2c_end() I2C_WIRE_CLASS.endTransmission()
+  #else // (I2C_Library_Preference == _I2C_Impl_AceWire)
+    #define mx_i2c_init() i2cAceWire.begin()
+    #define mx_i2c_start(address) i2cAceWire.beginTransmission(address)
+    #define mx_i2c_write(byte) i2cAceWire.write(byte)
+    #define mx_i2c_end() i2cAceWire.endTransmission()
+  #endif
 
 #endif
 
 #if defined(P8544)
+// this really does not belong here
   #include <pcd8544.h>
   #define ADMAX 1023
   #define ADPIN 0
--- MaxDuino/i2c_config.h
+++ MaxDuino/i2c_config.h
@@ -6,10 +6,22 @@
 
 // preferred I2C implementation
 #ifndef I2C_Library_Preference
-#define I2C_Library_Preference _I2C_Impl_SoftI2CMaster
-//#define I2C_Library_Preference _I2C_Impl_SoftWire
+#if defined(__AVR_ATmega2560__) || defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega328P__)
+    #define I2C_Library_Preference _I2C_Impl_AceWire
+//    #define I2C_Library_Preference _I2C_Impl_SoftI2CMaster
+#else
+    #define I2C_Library_Preference _I2C_Impl_Wire
+#endif
 #endif
 
-#define I2CFAST // if defined, I2C bus runs at 400kHz, instead of 100kHz default
+// I2CFAST, if defined, I2C bus runs at 400kHz, instead of 100kHz default
+#define I2CFAST
+
+// I2C_DELAY_MICROS, only used by Acewire initialisation
+// How many microseconds to wait between I2C requests
+// Default is calculated based on I2CCLOCK (which is calculated based on I2CFAST, above)
+// Defining here to be 0 means run as fast as possible (which is around 400kHz if using SimpleWireFastInterface on AVR)
+// If using acewire and I2C is locking up, you may want to comment out the below, or set to some other value > 0 
+#define I2C_DELAY_MICROS 0
 
 #endif // I2C_CONFIG_H_INCLUDED
